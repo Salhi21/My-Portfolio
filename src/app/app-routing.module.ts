81d294ea509f034170ca1b9be43f2be658aeb96d
Salhi Nassim <salhi.nassim.21@gmail.com>
2022-06-29 00:00:07 +0100

    brrrr

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [{
-  path : '',
+  path : 'https://my-portfolio-ez91qo7n7-salhi21.vercel.app',
   component : HomeComponent
 }];
 
